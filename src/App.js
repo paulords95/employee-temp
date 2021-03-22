4bbcf7dfd81a8f5a27d07274804c447330a3bdcc
paulords95 <paulords95@gmail.com>
2021-03-22 14:31:58 -0300

    new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 
 import EmployeeList from "./components/EmployeeList/EmployeeList";
+import Temperature from "./components/Temperatures/Temperature";
 
 function App() {
   return (
     <div className="App">
       <form className="mb-6 w-4/5" action="/" method="post">
         <EmployeeList />
+        <Temperature />
       </form>
     </div>
   );
---
+++ src/components/Temperatures/Temperature.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: "block",
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
+export default function Temperature() {
+  const classes = useStyles();
+
+  const handleChange = (event) => {};
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        margin: "0 auto",
+        paddingTop: "10vh",
+        paddingInline: "15vw",
+      }}
+    >
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">
+          Temperatura
+        </InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          onChange={handleChange}
+          defaultValue={36}
+        >
+          <MenuItem value={34}>34</MenuItem>
+          <MenuItem value={35}>35</MenuItem>
+          <MenuItem value={36}>36</MenuItem>
+          <MenuItem value={37}>37</MenuItem>
+          <MenuItem value={38}>38</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">
+          Temperatura
+        </InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          onChange={handleChange}
+        >
+          <MenuItem value={0}>0</MenuItem>
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+          <MenuItem value={8}>8</MenuItem>
+          <MenuItem value={9}>9</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
