6091d38830ef1888ef9274259fc62e413054ad53
paulords95 <paulords95@gmail.com>
2021-03-24 21:02:03 -0300

    nothing

---
+++ server/routes/storeTemperature.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+const { dbConnectSelect } = require("../db-connect");
+
+router.get("/insert-temp", async (req, res) => {});
+
+module.exports = router;
