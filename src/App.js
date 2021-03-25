2390d79d6392aac80e65d8620541212ce4ee2e35
paulords95 <paulords95@gmail.com>
2021-03-25 10:26:50 -0300

    added logo and tostify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import "./App.css";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import EmployeeList from "./components/EmployeeList/EmployeeList";
 import Temperature from "./components/Temperatures/Temperature";
 
+import logo from "./assets/logo2.png";
+
+import Button from "@material-ui/core/Button";
+
 function App() {
+  const notify = () => toast.success("Temperatura Salva");
   return (
     <div className="App">
-      <form className="mb-6 w-4/5" action="/" method="post">
+      <img className="logo" src={logo}></img>
+      <form
+        className="mb-6 w-4/5"
+        className="formTemp"
+        action="/"
+        method="post"
+      >
         <EmployeeList />
         <Temperature />
+
+        <Button
+          onClick={notify}
+          className="form-btn"
+          variant="contained"
+          color="primary"
+        >
+          Gravar
+        </Button>
+        <ToastContainer />
       </form>
     </div>
   );
