5a3938851e2d0a5a0d514b34c1848be1b0ed61d0
paulords95 <paulords95@gmail.com>
2021-03-27 20:21:34 -0300

    created new route

--- server/routes/storeTemperature.js
+++ server/routes/storeTemperature.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 
 const { dbConnectInsert } = require("../db-connect");
 
-router.get("/insert-temp", async (req, res) => {});
+router.get("/insert-temp", async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
 module.exports = router;
