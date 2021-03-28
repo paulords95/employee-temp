1bbeea9cf305e973440a2fafe7dbce061f79def1
paulords95 <paulords95@gmail.com>
2021-03-28 10:01:53 -0300

    added response to function

--- server/routes/storeTemperature.js
+++ server/routes/storeTemperature.js
@@ -4,10 +4,12 @@ const { dbConnectInsert } = require("../db-connect");
 
 const checkIfTempWasTaken = require("../middleware/checkIfTempWasTaken");
 
-const currentDate = require("../utils/formatDate");
+const currentDate = require("../utils/formatDate")();
 
 router.post("/store-temperature", checkIfTempWasTaken, async (req, res) => {
   try {
+    const { codUsu, codNameUsu, tmpAfe, horReg } = req.body;
+
     const insertQuery = `
     INSERT INTO USU_T577 (USU_CODEMP, USU_DATREG, USU_CODUSU, USU_NOMUSU, USU_TMPAFE, USU_HORREG) VALUES
   (
@@ -20,20 +22,9 @@ router.post("/store-temperature", checkIfTempWasTaken, async (req, res) => {
   )
   `;
 
-    const params = {
-      codUsu: req.body.codUsu,
-      codNameUsu: req.body.codNameUsu,
-      tmpAfe: parseFloat(req.body.tmpAfe),
-      horReg: req.body.horReg,
-    };
+    dbConnectInsert(insertQuery, codUsu, codNameUsu, tmpAfe, horReg);
 
-    dbConnectInsert(
-      insertQuery,
-      params.codUsu,
-      params.codNameUsu,
-      params.tmpAfe,
-      params.horReg
-    );
+    res.status(200).send("Temperatura registrada com sucesso");
   } catch (error) {
     console.error(error.message);
     res.status(500).json("server error");
