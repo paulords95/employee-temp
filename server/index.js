159c7edb3b2a4255306b0c70a42bc95fe1e9986a
paulords95 <paulords95@gmail.com>
2021-03-28 00:15:54 -0300

    added insert logic

--- server/index.js
+++ server/index.js
@@ -7,23 +7,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-//////
-const checkIfTempWasTaken = require("./middleware/checkIfTempWasTaken");
-
 //routes
 app.use("/employees", require("./routes/getEmployees"));
-
-app.get(
-  "/api/checkiftempwastaken/:codUsu",
-  checkIfTempWasTaken,
-  async (req, res) => {
-    try {
-      res.json("foi");
-    } catch (error) {
-      res.send("server error");
-    }
-  }
-);
 
 app.listen(process.env.PORT, () => {
   console.log(`server running at port ${process.env.PORT}`);
--- server/routes/storeTemperature.js
+++ server/routes/storeTemperature.js
@@ -2,9 +2,53 @@ const router = require("express").Router();
 
 const { dbConnectInsert } = require("../db-connect");
 
-router.get("/insert-temp", async (req, res) => {
+const checkIfTempWasTaken = require("../middleware/checkIfTempWasTaken");
+
+router.get("/store-temperature", checkIfTempWasTaken, async (req, res) => {
+  const d = new Date();
+  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+
+  const formatBr = d
+    .toLocaleDateString("pt-BR", options)
+    .split("-")
+    .reverse()
+    .join("/");
+
+  const newDate = new Date(formatBr);
+
   try {
-  } catch (error) {}
+    const insertQuery = `
+    INSERT INTO USU_T577 (USU_CODEMP, USU_DATREG, USU_CODUSU, USU_NOMUSU, USU_TMPAFE, USU_HORREG) VALUES
+  (
+    1,
+    TO_DATE('${formatBr}','DD/MM/YYYY'),
+    :codUsu,
+    (select usu_nomusu from usu_t522 where usu_codusu =:nomUsu),
+    :tmpAfe,
+    :horReg
+  )
+  `;
+
+    const params = {
+      codUsu: req.params.codUsu,
+      nomUsu: req.params.nomUsu,
+      tmpAfe: parseFloat(req.params.tmpAfe),
+      horReg: req.params.horReg,
+    };
+
+    dbConnectInsert(
+      req,
+      res,
+      insertQuery,
+      params.codUsu,
+      params.nomUsu,
+      params.tmpAfe,
+      params.horReg
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("server error");
+  }
 });
 
 module.exports = router;
