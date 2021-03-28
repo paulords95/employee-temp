e97a321512d9acd705e860216443c23e5c4883c4
paulords95 <paulords95@gmail.com>
2021-03-28 00:15:54 -0300

    added insert logic

--- server/middleware/checkIfTempWasTaken.js
+++ server/middleware/checkIfTempWasTaken.js
@@ -1,6 +1,17 @@
 module.exports = async (req, res, next) => {
   const { dbConnectInsert, dbConnectSelect } = require("../db-connect");
-  const checkRegister = `select * from usu_t577 where usu_datreg = TO_DATE('27/03/2021', 'DD/MM/YYYY') AND usu_codusu = :codUsu`;
+  const d = new Date();
+  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+
+  const formatBr = d
+    .toLocaleDateString("pt-BR", options)
+    .split("-")
+    .reverse()
+    .join("/");
+
+  const newDate = new Date(formatBr);
+
+  const checkRegister = `select * from usu_t577 where usu_datreg = TO_DATE('${formatBr}','DD/MM/YYYY')   AND usu_codusu = :codUsu`;
 
   try {
     const result = await dbConnectSelect(checkRegister, req.params.codUsu);
@@ -11,6 +22,6 @@ module.exports = async (req, res, next) => {
     next();
   } catch (error) {
     console.error(error.message);
-    return res.status(404).json("not taken");
+    return res.status(404).json("server error");
   }
 };
